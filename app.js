a5c3e3f10511878152a2e3371e3a387a7e1ffe4e
Swikar Gyawali <swikargyawali710@gmail.com>
2025-11-30 12:50:20 +0545

    project setup

---
+++ app.js
@@ -0,0 +1,29 @@
+const express= require('express');
+
+const app = express();
+
+
+//connecting a database
+const mongoose= require('mongoose');
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+
+});
+
+async function main(){
+    await moongoose.connect(MONGO_URL);
+}
+
+app.get("/",(req,res)=>{
+    res.send("Hi, I am root");
+})
+
+app.listen(8080,()=>{
+    console.log("server is listening to 8080");
+})
